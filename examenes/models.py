b52c423dc17c356c41ea83904c53ba97da43a54f
Royer G <royjuni3431@gmail.com>
2020-02-27 13:55:34 -0500

    Mejorando los modelos de Examenes

--- examenes/models.py
+++ examenes/models.py
@@ -1,20 +1,32 @@
 """Modelos de la app examenes"""
 # Django
 from django.db import models
+from usuarios.models import Perfil
 
 class Examen(models.Model):
-    titulo = models.CharField(max_length=255)
-    descripcion = models.TextField()
+    titulo = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=255, blank=True, null=True)
+    imagen = models.ImageField(upload_to='')
+    fecha_publicacion = models.DateTimeField('Fecha de publicación', auto_now_add=True)
+    fecha_modificacion = models.DateTimeField('Fecha de modificación', auto_now=True)
 
-    publicado = models.DateTimeField(auto_now_add=True)
-    modificado = models.DateTimeField(auto_now=True)
+    creado_por = models.ForeignKey('usuarios.Perfil', on_delete=models.SET_DEFAULT)
 
-class Pregunta(models.Model):
-    pregunta = models.CharField(max_length=255)
-    imagen = models.ImageField(null=True, blank=True)
+class Preguntas(models.Model):
+    enunciado = models.TextField()
+    imagen = models.ImageField()
+    examen = models.ForeignKey('Examen', on_delete=models.CASCADE)
+    fecha_publicacion = models.DateTimeField('Fecha de publicación', auto_now_add=True)
+    fecha_modificacion = models.DateTimeField('Fecha de modificación', auto_now=True)
+
+    creado_por = models.ForeignKey('usuarios.Perfil', on_delete=models.SET_DEFAULT)
 
 class Respuesta(models.Model):
-    pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
-    respuesta = models.CharField(max_length=200)
-    imagen = models.ImageField(null=True, blank=True)
-    selecionada = models.IntegerField(default=0)
+    enunciado = models.CharField(max_length=255)
+    imagen = models.ImageField()
+    pregunta = models.ForeignKey('Pregunta', on_delete=models.CASCADE)
+    correcta = models.BooleanField('Respuesta Correnta', default=False)
+    fecha_publicacion = models.DateTimeField('Fecha de publicación', auto_now_add=True)
+    fecha_modificacion = models.DateTimeField('Fecha de modificación', auto_now=True)
+
+    creado_por = models.ForeignKey('usuarios.Perfil', on_delete=models.SET_DEFAULT)
