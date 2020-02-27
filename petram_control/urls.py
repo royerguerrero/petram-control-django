1aa6ba53373c9f5bf72ef8ff30be5d9ce9df2232
Royer G <royjuni3431@gmail.com>
2020-02-27 13:55:34 -0500

    Mejorando los modelos de Examenes

--- petram_control/urls.py
+++ petram_control/urls.py
@@ -7,6 +7,7 @@ from django.urls import path
 from usuarios import views as usuarios_views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', usuarios_views.index, name='index'),
     path('recuperar-password/<int:usuario_id>', usuarios_views.forget_password, name='forget_password')
 ]
