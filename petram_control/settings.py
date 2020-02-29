bb762d3e0b15bc6d0cb47c6a80faac3d0c3ff126
Royer G <royjuni3431@gmail.com>
2020-02-28 21:04:10 -0500

    Primer template

--- petram_control/settings.py
+++ petram_control/settings.py
@@ -130,10 +130,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
 ]
-STATICFILES_FINDER = [
+
+STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
